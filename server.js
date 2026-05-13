42ec0a6fbd33764309825b96bf56a730999a94b8
inunesz <feliperecupero@gmail.com>
2026-05-13 13:56:52 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,68 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-var http = require('http');
-var express = require('express' ) ;
-var app = express () ;
-app. use(express.static('./public' ));
-var server = http.createServer(app);
-server.listen(80);
- console. log("servidor rodando. .. ") ;
- 
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/blogDB')
+.then(() => {
+    console.log('MongoDB conectado');
+})
+.catch((err) => {
+    console.log(err);
+});
+
+
+const Post = mongoose.model('Post', {
+
+    titulo: String,
+    resumo: String,
+    conteudo: String
+
+});
+
+
+app.get('/', (req, res) => {
+    res.redirect('/blog');
+});
+
+
+app.get('/blog', async (req, res) => {
+
+    const posts = await Post.find();
+
+    res.render('blog', {
+        posts: posts
+    });
+
+});
+
+
+app.post('/cadastra_post', async (req, res) => {
+
+    const novoPost = new Post({
+
+        titulo: req.body.titulo,
+        resumo: req.body.resumo,
+        conteudo: req.body.conteudo
+
+    });
+
+    await novoPost.save();
+
+    res.redirect('/blog');
+
+});
+
+
+app.listen(80, () => {
+    console.log('Servidor rodando na porta 80');
+});
